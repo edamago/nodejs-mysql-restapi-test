a94eecfaabdfe74ca7ac41a9b3b434ea7f3d33c0
edamago <daniel.martel@outlook.com>
2024-04-06 12:18:49 -0500

    se terminó api registro

--- src/controllers/registro.controller.js
+++ src/controllers/registro.controller.js
@@ -72,7 +72,7 @@ export const updateRegistro = async (req,res) => {
         const {id} =req.params
         const {nombre,DNI,direccion,email,celular,clave} = req.body
 
-        const [result] = await pool.query('Update registro set nombre = IFNULL(?,nombre) where userId = ?',[nombre])
+        const [result] = await pool.query('Update registro set nombre = IFNULL(?,nombre), DNI = IFNULL(?, DNI), direccion = IFNULL(?, direccion), email = IFNULL(?, email), celular = IFNULL(?, celular), clave = IFNULL(?, clave) where userId = ?',[nombre,DNI,direccion,email,celular,clave])
 
         if(result.affectedRows<=0) return res.status(404).json({message: 'Empleado no encontrado no se pudo nodificar'})
 
