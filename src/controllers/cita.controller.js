a0d09f00f6a2415101913532b00c74eec32ba3ea
edamago <daniel.martel@outlook.com>
2024-04-12 17:34:55 -0500

    api citas

---
+++ src/controllers/cita.controller.js
@@ -0,0 +1,14 @@
+import {pool} from '../db.js'
+
+export const getCitas = async(req,res) =>{
+
+   try {
+        //throw new Error('Mi error')
+        const [rows] = await pool.query('select a.id,a.fechaCita,a.especialidad,a.sede,a.medicoId,b.nombre,a.userId,r.nombre  from Cita a join medico b on b.id=a.medicoId join registro r on r.userId=a.userId',)
+        //res.json(rows) 
+        res.json({ results: rows }); 
+   } catch (error) {
+        return res.status(500).json({message:'Error en el get'})
+   }
+   
+}
---
+++ src/routes/cita.routes.js
@@ -0,0 +1,6 @@
+import {Router} from 'express'
+import {getCitas} from '../controllers/cita.controller.js'
+
+const router = Router()
+
+router.get('/citas',getCitas)
