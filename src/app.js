b39fcf0fbcacd0365fb29d8f90eae2e93abe6eef
edamago <daniel.martel@outlook.com>
2024-04-12 18:55:01 -0500

    api citas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import seguimientosRoutes from './routes/seguimiento.routes.js'
 import diariosRoutes from './routes/diario.routes.js'
 import medicionesRoutes from './routes/mediciones.routes.js'
 import medicosRoutes from './routes/medico.routes.js'
-import citasRoutes from './routes/cita.routes.js'
+
 
 import indexRoutes from './routes/index.routes.js'
 
@@ -22,7 +22,7 @@ app.use('/api',seguimientosRoutes)
 app.use('/api',diariosRoutes)
 app.use('/api',medicionesRoutes)
 app.use('/api',medicosRoutes)
-app.use('/api',citasRoutes)
+
 
 app.use((req,res,next)=>{
     res.status(404).json({message:"Ruta no encontrada"})
