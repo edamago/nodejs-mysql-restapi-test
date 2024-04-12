eb54fcbdba5cce1b90fb1b2b6f9e4b2da9ef0c6c
edamago <daniel.martel@outlook.com>
2024-04-12 18:59:26 -0500

    api citas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import seguimientosRoutes from './routes/seguimiento.routes.js'
 import diariosRoutes from './routes/diario.routes.js'
 import medicionesRoutes from './routes/mediciones.routes.js'
 import medicosRoutes from './routes/medico.routes.js'
-
+import citasRoutes from './routes/cita.routes.js'
 
 import indexRoutes from './routes/index.routes.js'
 
@@ -16,12 +16,14 @@ const app=express()
 app.use(express.json())
 
 app.use(indexRoutes)
+app.use('/api',citasRoutes)
 app.use('/api',employeesRoutes)
 app.use('/api',registrosRoutes)
 app.use('/api',seguimientosRoutes)
 app.use('/api',diariosRoutes)
 app.use('/api',medicionesRoutes)
 app.use('/api',medicosRoutes)
+
 
 
 app.use((req,res,next)=>{
